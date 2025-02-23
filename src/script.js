73fc312d3a932fd109dd0c01f7651e0ca84290c0
pwompus <dariusleong8@gmail.com>
2025-02-23 15:50:54 +1100

    attempts to detect overflow

--- src/script.js
+++ src/script.js
@@ -1,3 +1,11 @@
 window.onload = () => {
 	$("textarea").inputfit()
+
+	/*if ($("textarea").scrollWidth > $("textarea").innerWidth || $("textarea").scrollHeight > $("textarea").innerHeight) {
+		console.log("overflown!")
+	}
+
+	if ($("textarea").scrollWidth > $(window).width() || $("textarea").scrollHeight > $(window).height()) {
+		console.log("overflow")
+	}*/
 }
